ddecbc909e64eaa21b41ce3e8110350866b512f8
Razvan Boasca <6fed8jyt@students.codeinstitute.net>
2024-09-11 20:24:24 +0000

    create basic django settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!gtj1q5lr45tjl_%i24#g)e)gw)uojvg+y+_et2o8mbpr9wpd+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-rzvghub-djangoblog-z4qd5q0ryai.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = []
 
 
 # Application definition
